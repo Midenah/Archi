e18ba679c2f2f8e6d5a3c2e7cb4ca2f509bfc343
oluyomi justina <justinaolamide2611@gmail.com>
2024-01-30 21:39:18 +0100

    media

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let navbar =document.querySelector('.navbar');
 
 menu.onclick = () => {
     menu.classList.toggle("bx-x");
-    navbar.classList.toggle("open");
+    navbar.classList.toggle("show");
 }
